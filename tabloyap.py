6706aa855cfea1c4a8071a0c0040c7bdfb5e4fb4
1100110010 <75100630+1100110010@users.noreply.github.com>
2021-02-01 00:40:06 +0300

    Add files via upload

---
+++ tabloyap.py
@@ -0,0 +1,12 @@
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.field_names = ['isim', 'yaş', 'şehir']
+tabble.add_row(["falcon excalibur", 13, "ağrı"])
+tabble.add_row(["aeburuwole", 14, "ağrı"])
+tabble.add_row(["veteran7", 000000, "bilinmiyor"])
+tabble.add_row(["null", 123, "null"])
+tabble.add_row(["null", 123, "null"])
+
+print(table)
