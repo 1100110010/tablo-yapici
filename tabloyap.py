9cde44fde5d87fa3bb3401820186c9edb519b46d
1100110010 <75100630+1100110010@users.noreply.github.com>
2021-02-05 00:19:40 +0300

    Update tabloyap.py

--- tabloyap.py
+++ tabloyap.py
@@ -3,10 +3,10 @@ from prettytable import PrettyTable
 table = PrettyTable()
 
 table.field_names = ['isim', 'yaş', 'şehir']
-tabble.add_row(["falcon excalibur", 13, "ağrı"])
-tabble.add_row(["aeburuwole", 14, "ağrı"])
-tabble.add_row(["veteran7", 000000, "bilinmiyor"])
-tabble.add_row(["null", 123, "null"])
-tabble.add_row(["null", 123, "null"])
+table.add_row(["falcon excalibur", 13, "ağrı"])
+table.add_row(["aeburuwole", 14, "ağrı"])
+table.add_row(["veteran7", 000000, "bilinmiyor"])
+table.add_row(["null", 123, "null"])
+table.add_row(["null", 123, "null"])
 
 print(table)
