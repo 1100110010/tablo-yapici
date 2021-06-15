179cc60797b14d80df691eed7907191d45578f78
1100110010 <75100630+1100110010@users.noreply.github.com>
2021-06-15 10:13:07 +0300

    tabloyap.py

--- tabloyap.py
+++ tabloyap.py
@@ -3,11 +3,11 @@ from prettytable import PrettyTable
 table = PrettyTable()
 
 table.field_names = ['isim', 'yaş', 'şehir']
-table.add_row(["falcon excalibur", 13, "ağrı"])
-table.add_row(["aeburuwole", 14, "ağrı"])
-table.add_row(["veteran7", 000000, "bilinmiyor"])
+table.add_row(["djdjfj", 13, "dısjsksmsn"])
+table.add_row(["djdjwjejej", 14, "fjfjdjsn"])
+table.add_row(["dkwkwksjs", 000000, "bilinmiyor"])
 table.add_row(["null", 123, "null"])
 table.add_row(["null", 123, "null"])
 
 print(table)
-print('made by falcon excalibur(can cayar)')
+
